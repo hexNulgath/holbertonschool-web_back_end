75f581f0ec49e73f1a2b610f031db624d81e7743
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:06:20 -0300

    added static method

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
     constructor(amount, currency) {
@@ -36,5 +36,9 @@ export default class Pricing {
 
     displayFullPrice() {
         return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
     }
 }
