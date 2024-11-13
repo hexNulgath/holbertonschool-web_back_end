ae890bf04f67b450196990cecf13b4577d2afb76
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:26:00 -0300

    testing

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft === 'number') { // 'number' in lowercase
+    if (typeof sqft === 'number') {
       this._sqft = sqft;
     } else {
       throw new TypeError('sqft must be a number');
-    }
-
-    // Check if the subclass has overridden evacuationWarningMessage
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
