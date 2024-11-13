82477247f885c1bef0a8e3c3378e02c3288b6b32
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 16:25:07 -0300

    lint spaces

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,17 +1,17 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        if (typeof size !== 'number' || typeof location !== 'string') {
-            throw new TypeError('values not correct type');
-        }
-        this._size = size;
-        this._location = location;
+  constructor(size, location) {
+    if (typeof size !== 'number' || typeof location !== 'string') {
+      throw new TypeError('values not correct type');
     }
+    this._size = size;
+    this._location = location;
+  }
 
-    valueOf() {
-        return this._size;
-    }
+  valueOf() {
+    return this._size;
+  }
 
-    toString() {
-        return this._location;
-    }
+  toString() {
+    return this._location;
+  }
 }
