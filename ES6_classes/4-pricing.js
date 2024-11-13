50cd14ac8f7166d8b4d0e237c9ad68360ecd2902
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:03:44 -0300

    created

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount === 'number' && currency instanceof Currency) {
+            this._amount = amount;
+            this._currency = currency;
+        } else {
+            throw new TypeError('Amount must be a number and currency must be a currency');
+        }
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set amount(value) {
+        if (typeof value === 'number') {
+            this._amount = value;
+        } else {
+            throw new TypeError('amount must be a number');
+        }
+    }
+
+    set currency(value) {
+        if (value instanceof Currency) {
+            this._currency = value;
+        } else {
+            throw new TypeError('currency must be a currency');
+        }
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    }
+}
