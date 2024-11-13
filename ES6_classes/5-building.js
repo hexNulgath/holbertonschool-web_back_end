f3adc396ec50e725caf90aca7ca88d6421828c92
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:24:06 -0300

    test

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate an abstract class.');
-    }
     if (typeof sqft === 'number') {
       this._sqft = sqft;
     } else {
       throw new TypeError('sqft must be a Number');
+    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
