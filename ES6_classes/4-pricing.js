b95092b384805687768639358d1febc54c51ff92
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:07:24 -0300

    lint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,44 +1,44 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        if (typeof amount === 'number' && currency instanceof Currency) {
-            this._amount = amount;
-            this._currency = currency;
-        } else {
-            throw new TypeError('Amount must be a number and currency must be a currency');
-        }
+  constructor(amount, currency) {
+    if (typeof amount === 'number' && currency instanceof Currency) {
+      this._amount = amount;
+      this._currency = currency;
+    } else {
+      throw new TypeError('Amount must be a number and currency must be a currency');
     }
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    get currency() {
-        return this._currency;
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('amount must be a number');
     }
+  }
 
-    set amount(value) {
-        if (typeof value === 'number') {
-            this._amount = value;
-        } else {
-            throw new TypeError('amount must be a number');
-        }
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(value) {
-        if (value instanceof Currency) {
-            this._currency = value;
-        } else {
-            throw new TypeError('currency must be a currency');
-        }
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('currency must be a currency');
     }
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${this._currency._name} (${this._currency._code})`;
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
