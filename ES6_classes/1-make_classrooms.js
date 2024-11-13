3a274b4cfb7fb2794afbede1ae87d95269cabe65
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 11:08:24 -0300

    added export

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from "./0-classroom";
 
-function initializeRooms() {
+export default function initializeRooms() {
   const school = [];
-  school.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34))
+  school.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34));
   return school;
 }
