1c18a19e5da6fdb538f95317a27791ec8521ef18
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 11:04:45 -0300

    missing brakets

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from "./0-classroom";
 
-function initializeRooms{
+function initializeRooms() {
   const school = [];
   school.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34))
   return school;
