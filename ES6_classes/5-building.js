a84f006289327bfb694189603c962d4a2006417a
hexNulgath <machadoignacio@hotmail.com>
2024-11-13 15:16:11 -0300

    created

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate an abstract class.');
+    }
+    if (typeof sqft === 'Number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('sqft must be a Number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
