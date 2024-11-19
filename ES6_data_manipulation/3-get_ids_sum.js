903cda7f1b050712fda9530215da601e627a4d37
hexNulgath <machadoignacio@hotmail.com>
2024-11-19 14:45:58 -0300

    eslint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,8 @@
 export default function getStudentIdsSum(students) {
-  const studentSum = students.reduce((accumulator, student) => {
-    return accumulator + student.id;
-  }, 0)
-  return studentSum;
+  if (!Array.isArray(students)) {
+    return 0; // Return 0 if students is not an array
+  }
+
+  const studentSum = students.reduce((accumulator, student) => accumulator + student.id, 0); // Concise arrow function
+  return studentSum; // Semicolon added
 }
