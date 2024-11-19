65c22c5f032678e5ba18f8ee9648ae28c2839026
hexNulgath <machadoignacio@hotmail.com>
2024-11-19 20:11:32 -0300

    if not a map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(map) {
+  if (typeof map !== Map) {
+    throw new Error('Cannot process');
+
+  }
   for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
