9b3158b215526ea0be8fb88e41853ba7850fca77
hexNulgath <machadoignacio@hotmail.com>
2024-11-19 15:25:40 -0300

    return data view

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,6 +6,8 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
   int8Array[position] = value;
+  const dataView = new DataView(buffer);
 
-  return int8Array;
+
+  return dataView;
 }
