544256fcffea447d4199cbfb5015cd109d241fff
hexNulgath <machadoignacio@hotmail.com>
2024-11-19 15:22:10 -0300

    created task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length - 1) {
+    throw new Error("Position outside range");
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+
+  return int8Array;
+}
