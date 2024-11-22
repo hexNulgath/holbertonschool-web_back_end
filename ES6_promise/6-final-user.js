082cbcb9a5ebc7956d7cc0eb4b57daba2acdf53f
hexNulgath <machadoignacio@hotmail.com>
2024-11-22 12:46:33 -0300

    lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise]).then(results => {
-    return results.map(result => ({
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
+    return results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled'
-        ? result.value // This will be the user object
-        : `Error: ${result.reason.message}` // This will be the error message
+      value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
     }));
   });
 }
