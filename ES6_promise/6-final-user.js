9ed062a39ba42d5d50e746372c99f3999016c51f
hexNulgath <machadoignacio@hotmail.com>
2024-11-22 12:44:41 -0300

    no async

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  const results = await Promise.allSettled([signUpPromise, uploadPromise]);
-
-  return results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled'
-      ? result.value // This will be the user object
-      : `Error: ${result.reason.message}` // This will be the error message
-  }));
+  return Promise.allSettled([signUpPromise, uploadPromise]).then(results => {
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled'
+        ? result.value // This will be the user object
+        : `Error: ${result.reason.message}` // This will be the error message
+    }));
+  });
 }
