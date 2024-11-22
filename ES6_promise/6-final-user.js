417975db0f1432c23397a7ce182f594613dd3d45
hexNulgath <machadoignacio@hotmail.com>
2024-11-22 12:18:13 -0300

    diferent error message

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
+
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
+
   const results = await Promise.allSettled([signUpPromise, uploadPromise]);
 
   return results.map(result => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
+    value: result.status === 'fulfilled' ? result.value : result.reason.message
   }));
 }
