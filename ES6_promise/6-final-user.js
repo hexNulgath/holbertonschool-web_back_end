564e0968d0f79d83f4737bc505f5811ac9ade10e
hexNulgath <machadoignacio@hotmail.com>
2024-11-22 12:49:25 -0300

    lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
-    return results.map((result) => ({
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) =>
+    results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
-    }));
-  });
+    }))
+  );
 }
