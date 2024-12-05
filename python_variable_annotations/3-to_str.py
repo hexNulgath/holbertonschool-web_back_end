6470b69d5b90a9afc07fbcea61b2ea9ce7da00ff
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:26:54 -0300

    created task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to convert a float number to a
+string representation, rounded to two decimal places.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to a string representation,
+    rounded to two decimal places.
+    """
+    return "{:.2f}".format(n)
