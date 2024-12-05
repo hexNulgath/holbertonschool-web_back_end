abf00a8a2dcecd469aaee5f517969d2c83186251
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:23:21 -0300

    created task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the floor of a float number.
+"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float number."""
+    return int(n)
