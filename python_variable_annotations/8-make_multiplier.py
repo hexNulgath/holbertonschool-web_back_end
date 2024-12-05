b5c3aec07b578b5e9e33ed44c26a15e6020cbd08
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 15:04:57 -0300

    task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to create a multiplier function.
+
+The `make_multiplier` function returns another function that multiplies
+a given number by a specified multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Create a function that multiplies a given number.
+
+    Args:
+        multiplier (float): The number to multiply by.
+
+    Returns:
+        Callable[[float], float]: A function that multiplies
+        a given number by the multiplier.
+
+    Example:
+        >>> multiply_by_3 = make_multiplier(3)
+        >>> multiply_by_3(10)
+        30.0
+    """
+    def multiply_by_multiplier(x: float) -> float:
+        """
+        Multiply the given number by the multiplier.
+
+        Args:
+            x (float): The number to be multiplied.
+
+        Returns:
+            float: The result of multiplying `x` by `multiplier`.
+        """
+        return x * multiplier
+
+    return multiply_by_multiplier
