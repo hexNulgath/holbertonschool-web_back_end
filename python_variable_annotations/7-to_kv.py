cfe0264129d5d4063e6beb7562367cd8c65d5e52
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:58:01 -0300

    created task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""
+  This module provides a function to convert a key-value pair to a tuple.
+  """
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Converts a key-value pair to a tuple"""
+    return (k, float(v*v))
