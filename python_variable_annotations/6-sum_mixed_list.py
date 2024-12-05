ca5112a4767210788fdd343a383794912a4526df
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:49:52 -0300

    created task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to calculate the
+sum of all elements in a mixed list.
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+    """
+    Return the sum of all elements in a mixed list.
+
+    Args:
+        mxd_lst (List[int | float]): A list containing integers and/or floats.
+
+    Returns:
+        float: The sum of all elements in the list.
+    """
+    return sum(mxd_lst)
