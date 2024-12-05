286432cd170e26f24a0b19b83a541aa7a8c7cfc3
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:06:05 -0300

    created files

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    This function returns the sum of two floats.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
