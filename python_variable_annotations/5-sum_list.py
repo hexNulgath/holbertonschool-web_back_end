aac15e067473e8273a7356cf707364a2103f1642
hexNulgath <machadoignacio@hotmail.com>
2024-12-05 14:38:37 -0300

    created task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+from typing import List
+"""
+Function to sum a list of floats
+
+Args:
+    input_list (List[float]): A list of floats
+
+Returns:
+    float: The sum of the input list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floats
+
+    Args:
+        input_list (List[float]): A list of floats
+
+    Returns:
+        float: The sum of the input list
+    """
+    return sum(input_list)
