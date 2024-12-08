9e024c0a853807fab3aae5044342978d56094deb
hexNulgath <machadoignacio@hotmail.com>
2024-12-08 11:25:18 -0300

    created all files

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+"""
+Define an asynchronous function wait_random that takes an optional
+integer max_delay as a parameter.
+The function should sleep for a random time between 0 and max_delay seconds,
+and then return a random float value.
+
+If no max_delay is provided, the function should default to a
+maximum delay of 10 seconds.
+
+Your function should be implemented using asyncio.sleep and random.randint.
+"""
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''
+    Wait for a random time between 0 and max_delay (inclusive)
+    '''
+    await asyncio.sleep(random.randint(0, max_delay))
+    return random.random()
