05d73ae9b3dc834340dfc46a1cc3a0eb08b5e168
hexNulgath <machadoignacio@hotmail.com>
2024-12-08 12:15:02 -0300

    task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module defines a function to create an asyncio
+task that waits for a random time.
+""
+
+from typing import asyncio
+
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create an asyncio task that waits for a random time
+    between 0 and max_delay (inclusive)
+    """
+    return asyncio.create_task(wait_random(max_delay))
