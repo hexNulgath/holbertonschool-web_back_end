c8523faa7a9af30d868fd72239b40984e9e92e77
hexNulgath <machadoignacio@hotmail.com>
2024-12-08 11:29:09 -0300

    return float

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-
-import asyncio
-import random
 """
 Define an asynchronous function wait_random that takes an optional
 integer max_delay as a parameter.
@@ -13,11 +10,13 @@ maximum delay of 10 seconds.
 
 Your function should be implemented using asyncio.sleep and random.randint.
 """
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     '''
     Wait for a random time between 0 and max_delay (inclusive)
     '''
-    await asyncio.sleep(random.randint(0, max_delay))
+    await asyncio.sleep(random.uniform(0, max_delay))
     return random.random()
