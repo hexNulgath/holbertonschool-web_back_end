6c0882c05ad620793f9fd17df0f6cbf6c1bf75e2
hexNulgath <machadoignacio@hotmail.com>
2024-12-10 10:34:10 -0300

    created task 0 and empty files

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Asynchronously generates a sequence of 10 random floating-point
+numbers between 1 and 10.#+
+This function is a coroutine that uses the `async` keyword and the
+`await` keyword to#+
+yield random numbers asynchronously. It uses the `asyncio.sleep()`
+function to introduce#+
+a delay of 1 second between each number generation.#+
+
+Parameters:
+None
+
+Returns:
+float: A random floating-point number between 1 and 10.#+
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> None:
+    """Generates random numbers"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
