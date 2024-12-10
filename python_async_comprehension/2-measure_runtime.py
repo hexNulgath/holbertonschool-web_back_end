c969e49dd227e31fa5c87173601d5c5135e7fb66
hexNulgath <machadoignacio@hotmail.com>
2024-12-10 10:53:59 -0300

    task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+Measures the runtime of an asynchronous function.
+
+This function uses the asyncio library to measure the runtime
+of the provided asynchronous function. It uses asyncio.gather()
+to execute the function multiple times in parallel.
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime"""
+    start_time = time.time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                   async_comprehension(), async_comprehension())
+    end_time = time.time()
+    return end_time - start_time
