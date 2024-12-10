5f739e8df8c8e8a52bdd679d03b45e07c0852336
hexNulgath <machadoignacio@hotmail.com>
2024-12-10 10:37:00 -0300

    range from 0 not 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -18,8 +18,8 @@ import asyncio
 import random
 
 
-async def async_generator() -> None:
+async def async_generator():
     """Generates random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(1, 10)
+        yield random.uniform(0, 10)
