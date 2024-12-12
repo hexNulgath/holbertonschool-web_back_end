c6f07d39a59424ac3e8b443eb84d292654965d4a
hexNulgath <machadoignacio@hotmail.com>
2024-12-12 09:00:28 -0300

    style

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -14,7 +14,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the runtime"""
     start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                async_comprehension(), async_comprehension())
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension())
     end_time = time.time()
     return end_time - start_time
