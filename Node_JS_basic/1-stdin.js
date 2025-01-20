f49b97830fd238d543c72dd587fd69cc05d9d5da
hexNulgath <machadoignacio@hotmail.com>
2025-01-20 18:46:27 -0300

    using readline

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,17 @@
-process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+const readline = require('readline');
+
+const userInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-process.stdin.on('end', () => {
+
+console.log('Welcome to Holberton School, what is your name?');
+
+userInput.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  userInput.close();
+});
+
+userInput.on('close', () => {
   console.log('This important software is now closing');
 });
