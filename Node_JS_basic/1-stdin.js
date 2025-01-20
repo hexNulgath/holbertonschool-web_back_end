41ce5613c6d4bc0ebb192715bdfbf3a0c5d712fe
hexNulgath <machadoignacio@hotmail.com>
2025-01-20 18:36:39 -0300

    lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 function stdin() {
-    process.stdin.setEncoding('utf8');
-    console.log(`Welcome to Holberton School, what is your name?`);
-    process.stdin.on('data', (data) => {
-        console.log(`Your name is: ${data.trim()}`);
-    });
-    process.stdin.on('end', () => {
-        console.log('This important software is now closing');
-    });
+  process.stdin.setEncoding('utf8');
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.trim()}`);
+  });
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
 }
 
 module.exports = stdin;
