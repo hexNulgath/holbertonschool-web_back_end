e4d728b0c798839dc0c9047c007aba515318bcb8
hexNulgath <machadoignacio@hotmail.com>
2025-01-20 19:03:33 -0300

    use quesdtion method

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,11 +5,9 @@ const userInput = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-userInput.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  userInput.close();
+userInput.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
 });
 
 userInput.on('close', () => {
