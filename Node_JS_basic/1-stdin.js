bebed70123c6bdc83d9ecbbf4f20cafb1d906f20
hexNulgath <machadoignacio@hotmail.com>
2025-01-26 02:13:35 -0300

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
 const readline = require('readline');
 
 const userInput = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+ input: process.stdin,
+ output: process.stdout,
 });
 
 userInput.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}\r`);
-  userInput.close();
+ console.log(`Your name is: ${name}\r\n`);
+ userInput.close();
 });
 
 userInput.on('close', () => {
+ // Removed close event logging
 });
