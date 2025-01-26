10afe90c28cf4b45add901d03767692b14f3828d
hexNulgath <machadoignacio@hotmail.com>
2025-01-26 03:17:32 -0300

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,15 @@
 const readline = require('readline');
 
-const askQuestion = (query) => {
-  const userInput = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const userInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  return new Promise((resolve) => {
-    userInput.question(query, (answer) => {
-      resolve(answer);
-      userInput.close();
-    });
-  });
-};
+userInput.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  process.stdout.write(`Your name is: ${name}\r`);
+  userInput.close();
+});
 
-const main = async () => {
-  console.log('Welcome to Holberton School, what is your name?');
-  const name = await askQuestion('');
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-};
-
-main();
+userInput.on('close', () => {
+  process.stdout.write('\nThis important software is now closing');
+});
