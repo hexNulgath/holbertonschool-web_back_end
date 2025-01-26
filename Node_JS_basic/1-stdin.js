236beb218c7eac8d5ba53435c3847399303f267c
hexNulgath <machadoignacio@hotmail.com>
2025-01-26 02:48:44 -0300

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
 const readline = require('readline');
 
-
 const userInput = readline.createInterface({
- input: process.stdin,
- output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 userInput.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}\r`.padEnd(process.stdout.columns, ' ') + '\r');
+  console.log(`Your name is: ${name}`);
   userInput.close();
 });
 
 userInput.on('close', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
