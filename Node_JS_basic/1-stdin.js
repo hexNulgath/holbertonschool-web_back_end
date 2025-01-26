971fadc7861284c1c14fae43f67934a438861258
hexNulgath <machadoignacio@hotmail.com>
2025-01-26 03:03:07 -0300

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,10 +6,10 @@ const userInput = readline.createInterface({
 });
 
 userInput.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}`);
   userInput.close();
 });
 
 userInput.on('close', () => {
-  process.stdout.write('\nThis important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
